9f0b013e419f2192d3b3d46e8483a34aff3b7181
Lenny Ng'ang'a <codewithlennylen254@gmail.com>
2025-09-25 10:39:31 +0300

    slam not working

--- robot2025_sim/launch/slam.launch.py
+++ robot2025_sim/launch/slam.launch.py
@@ -1,13 +1,31 @@
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='slam_toolbox',
+#             executable='online_async_node',
+#             name='slam_toolbox',
+#             output='screen',
+#             parameters=[{'use_sim_time': True}]
+#         )
+#     ])
+
+
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+
+        # Run SLAM Toolbox online sync mode
         Node(
-            package='slam_toolbox',
-            executable='online_async_node',
-            name='slam_toolbox',
-            output='screen',
-            parameters=[{'use_sim_time': True}]
+            package="slam_toolbox",
+            executable="sync_slam_toolbox_node",
+            name="slam_toolbox",
+            output="screen",
+            parameters=["/home/lenny/Documents/GitHub/rdj/baseline_robot_ws/src/robot2025_sim/config/slam_params.yaml"],
         )
     ])
